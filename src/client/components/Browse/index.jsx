75d9096d62ef30a04ff207318b3f2e4b67a2e631
j0101 <cavnav@gmail.com>
2020-02-18 08:53:00 +0300

    browse

--- src/client/components/Browse/index.jsx
+++ src/client/components/Browse/index.jsx
@@ -5,79 +5,127 @@ import './styles.css';
 
 export function Browse({ props }) {
   const stateInit = {
-    photos: [],
-    isPhotosGetted: false,
-    thWidth: 100,
-    thHeight: 100,
+    previewWidth: 100,
+    previewHeight: 100,
     curPhotoInd: -1,
     curPhotoRotateDeg: 0,
-    forReact: 0,
+    componentClass: 'browse',
+    curPhotoHeight: undefined,
+    curPhotoWidth: undefined,
+    addRenderCompleted: false,
   };
 
   const [state, setState] = useState(stateInit);
+  const [photos, setPhotos] = useState([]);
 
   useEffect(() => {
     document.addEventListener('keydown', onKeyDown);
+
+    if (!state.addRenderCompleted && state.curPhotoInd > -1) {
+      const curPhoto = document.querySelector(`[src='${photos[state.curPhotoInd]}']`);
+      const { width, height } = getFitSize(curPhoto.getBoundingClientRect());
+      
+      setState({
+        ...state, 
+        curPhotoHeight: height,
+        curPhotoWidth: width,
+        addRenderCompleted: true,
+      });
+    }
 
     return () => {
       document.removeEventListener('keydown', onKeyDown);
     };
   });
 
-  !state.isPhotosGetted && serverApi({
-    props: {
-      url: 'browsePhotos'
-    }
-  })
+  useEffect(() => {
+    serverApi({
+      props: {
+        url: 'browsePhotos'
+      }
+    })
     .then(res => res.json())
     .then((res) => {
-      setState({
-        ...state,
-        photos: res.photos,
-        isPhotosGetted: true,
-      });
+      setPhotos(res.photos);
     });
+  }, []);
 
   let toRender = null;
+
   if (state.curPhotoInd === -1) {
-    toRender = state.photos.map((photo, ind) => <img key={photo} ind={ind} src={photo} width={state.thWidth} height={state.thHeight} />);
+    toRender = photos.map((photo, ind) => {
+      return (
+        <div 
+          key={photo}
+          className='fitPreview100'
+          style={{ 'backgroundImage': `url(${photo})` }}
+          ind={ind} 
+          src={photo}
+        >
+        </div>
+      );
+    });
   }
   if (state.curPhotoInd !== -1) {
-    const photo = state.photos[state.curPhotoInd];
-    toRender = <img key={photo} className="fitScreen" style={{transform: `rotate(${state.curPhotoRotateDeg}deg)`}} ind={state.curPhotoInd} src={photo} />;
+    const photo = photos[state.curPhotoInd];
+    toRender = (
+      <img 
+        key={photo} 
+        src={photo}        
+        style={{
+          transform: `rotate(${state.curPhotoRotateDeg}deg)`,
+          width: state.curPhotoWidth,
+          height: state.curPhotoHeight,
+        }} 
+        ind={state.curPhotoInd} 
+      />
+    );
   }
 
   return (
-    <div className="browse" onDoubleClick={onDblClickPhoto}>
+    <div className={state.componentClass} onDoubleClick={onDblClickPhoto}>
       { toRender }
-    </div>
+    </div>    
   );
 
   // --------------------------------------------------------------------
   function onDblClickPhoto(e) {
+    const curPhoto = e.target;
+    const { width, height } = getFitSize(curPhoto.getBoundingClientRect());
+    
     setState({
       ...state,
+      curPhotoHeight: height,
+      curPhotoWidth: width,
       curPhotoInd: +e.target.getAttribute('ind'),
+      componentClass: 'fitScreen',
     });
   }
 
   function onKeyDown(e) {
+    const prevPhotoInd = state.curPhotoInd > 0 ? state.curPhotoInd - 1 : 0;
+    const nextPhotoInd = state.curPhotoInd < photos.length - 1 ? (state.curPhotoInd + 1) : (photos.length - 1);
+    
+    const stateUpd = { ...state };
     switch (e.which) {
-      case 37: getPhoto({ curPhotoInd: state.curPhotoInd - 1 }); break; // prev
-      case 39: getPhoto({ curPhotoInd: state.curPhotoInd + 1 }); break; // next
-      case 38: getPhoto({ curPhotoRotateDeg: state.curPhotoRotateDeg + 90 }); break; // rotate right
-      case 40: getPhoto({ curPhotoRotateDeg: state.curPhotoRotateDeg - 90 }); break; // rotate left
+      case 37: stateUpd.curPhotoInd = prevPhotoInd; break; // prev
+      case 39: stateUpd.curPhotoInd = nextPhotoInd; break; // next
+      case 38: stateUpd.curPhotoRotateDeg = stateUpd.curPhotoRotateDeg + 90; break; // rotate right
+      case 40: stateUpd.curPhotoRotateDeg = stateUpd.curPhotoRotateDeg - 90; break; // rotate left
     }
+
+    setState(stateUpd);
   }
 
-  function getPhoto({ 
-    curPhotoInd = state.curPhotoInd, 
-    curPhotoRotateDeg = state.curPhotoRotateDeg,
-  }) {
-    setState({
-      ...state,
-      curPhotoInd,
-      curPhotoRotateDeg,
-    });
+  function getFitSize({ width, height }) {
+    const res = {};
+    if (width > height * 2) {
+      res.width = '100%';
+      res.height = undefined;
+    } else {
+      res.width = undefined;
+      res.height = '100%';
+    }
+    return res;
   }
 }
