3753785848f20843469509a7a00d9c30bb4e2c44
j0101 <cavnav@gmail.com>
2020-02-25 11:53:00 +0300

    copyStepWizard

--- src/client/components/Copy/index.js
+++ src/client/components/Copy/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Progress } from 'antd';
+import { Progress, } from 'antd';
 import { serverApi } from '../../serverApi';
 
 import 'antd/dist/antd.css';
@@ -8,6 +8,7 @@ import './styles.css';
 export function Copy({ props }) {
   const stateInit = {
     isCountGetted: false,
+    stepNum: 0,
     copyProgress: 0,
     countNewPhotos: 0,
   };
@@ -28,30 +29,57 @@ export function Copy({ props }) {
       });
     });
 
-  return (
-    <div className="copy">
-      Открой крышку и нажми пальцем на синюю карту памяти, вдавив внутрь, отпусти.
-      <img src="public/wizardCopy/001_getOutMemCard.jpg"></img><br />
-
-      Вставить карту памяти в кардРидер, как показано ниже:
-      <img src="public/wizardCopy/002_insertIntoCardReader.jpg"></img><br />
-    
-      Вставить кардРидер в системный блок, как показано ниже, чтобы совпал ключ.
-      <img src="public/wizardCopy/004_plugInPC.jpg"></img><br />
-      
-      <div className="flex flexDirColumn">
+  const arr = [
+    {
+      photoSrc: 'public/wizardCopy/001_getOutMemCard.jpg',
+      desc: 'нажми пальцем на синюю карту памяти, и, вдавив внутрь, отпусти.',
+    }, {
+      photoSrc: 'public/wizardCopy/002_insertIntoCardReader.jpg',
+      desc: 'Вставь карту памяти в кардРидер, как показано ниже:',
+    }, {
+      photoSrc: 'public/wizardCopy/004_plugInPC.jpg',
+      desc: 'Вставь кардРидер в системный блок, как показано ниже, чтобы совпал ключ.',
+    }, {    
+      toRender: <div className="flex flexDirColumn">
         Количество новых фото:
         { state.countNewPhotos }
         <input type="button" onClick={onCopy} value="Копировать" />
         <Progress type="circle" percent={state.copyProgress} />
-      </div>
+      </div>,
+    }, {
+      photoSrc: 'public/wizardCopy/005_returnMemCardInPhoto.jpg',
+      desc: 'После завершения копирования вытащить карту памяти из кардРидера и всавить обратно в фотоаппарат до щелчка, как показано ниже:',
+    }
+  ];
 
-      После завершения копирования вытащить карту памяти из кардРидера и всавить обратно в фотоаппарат до щелчка, как показано ниже:
-      <img src="public/wizardCopy/005_returnMemCardInPhoto.jpg" /><br />
+  const step = createStep();
+
+  return (
+    <div className="copy">      
+      { step }
+      <input type="button" onClick={onClickNextStep} value="Следующий шаг" /> 
     </div>
   );
 
   // ------------------------------------------------------------------
+  function onClickNextStep() {
+    setState({
+      ...state,
+      stepNum: state.stepNum + 1,
+    });
+  }
+  
+  function createStep() {
+    const step = arr[state.stepNum];
+    const content = []; 
+
+    step.desc && content.push(<div>{step.desc}</div>);
+    step.photoSrc && content.push(<img className="copyWizardImg" src={step.photoSrc} />);
+    step.toRender && content.push(step.toRender);
+
+    return <div className="step">{ content }</div>;
+  }
+
   function onCopy() {
     serverApi({
       props: {
